785dd8b9bc2aaae682f19777492fa3c073b6bf13
River Manoukian <river.m.phoenix@gmail.com>
2025-05-31 08:11:31 +1000

    updated Rivers Program.cs

--- river/Program.cs
+++ river/Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "This is Rivers server, Welcome");
-app.MapGet("/1", () => "OK");
+
 
 app.Run();
