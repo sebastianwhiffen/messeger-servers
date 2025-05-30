6de549191dfe49a01fbfb28108dbb34192c412f7
River Manoukian <river.m.phoenix@gmail.com>
2025-05-30 20:11:34 +1000

    Rinsed the river folder

--- river/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("goodbye, World!");
-
-
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine($"Hello, River {i}");
-}
