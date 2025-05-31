43b01110b5b0f186cec4810ef3c946d8936f4ee2
sebastianwhiffen <sebastianwhiffen@gmail.com>
2025-05-31 12:57:08 +1000

    start of rivers server

--- river/Program.cs
+++ river/Program.cs
@@ -1,7 +1,24 @@
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "This is Rivers server, Welcome");
+app.MapPost("/", async (HttpRequest request) =>
+{
+    using var reader = new StreamReader(request.Body, Encoding.UTF8);
+    var body = await reader.ReadToEndAsync();
+    Console.WriteLine(body);
+    return Results.Ok();
+});
 
+app.MapGet("/send-message/{message}", async (string message) =>
+{
+    var httpClient = new HttpClient();
+    var content = new StringContent(message, Encoding.UTF8, "application/json");
+    await httpClient.PostAsync("http://localhost:3000/", content);
+});
 
 app.Run("http://localhost:3000");
+
+
+
