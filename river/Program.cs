fdf6d8b099e71014d7f811f2d26d8a121aa49d50
River Manoukian <river.m.phoenix@gmail.com>
2025-05-31 14:01:15 +1000

    Moved anonymous funcs into named funcs

--- river/Program.cs
+++ river/Program.cs
@@ -3,20 +3,23 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapPost("/", async (HttpRequest request) =>
+async Task<IResult> HandlePost(HttpRequest request)
 {
     using var reader = new StreamReader(request.Body, Encoding.UTF8);
     var body = await reader.ReadToEndAsync();
     Console.WriteLine(body);
     return Results.Ok();
-});
+}
+app.MapPost("/", HandlePost);
 
-app.MapGet("/send-message/{message}", async (string message) =>
+async Task MSG_Get(string message)
 {
     var httpClient = new HttpClient();
     var content = new StringContent(message, Encoding.UTF8, "application/json");
     await httpClient.PostAsync("http://localhost:3000/", content);
-});
+}
+app.MapGet("/send-message/{message}", MSG_Get);
+
 
 app.Run("http://localhost:3000");
 
