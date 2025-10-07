106a1ac25e13c7bdc2a3030c4ece163d73f0b8f2
swhiff <sebastianwhiffen@gmail.com>
2025-10-07 13:40:28 +1100

    change to c#

---
+++ seb/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var users = new[]
+{
+    "river", "seb"
+};
+
+app.MapGet("/get-users", () => { return users; });
+app.Run();
+
